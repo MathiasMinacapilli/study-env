34d027a66d94c6a699811cb826fb92407fe75525
mathias <mathias.minacapilli@mercadolibre.com>
2021-02-07 16:40:59 -0300

    feature: add list component to personal area page

--- src/pages/personal-area/PersonalArea.tsx
+++ src/pages/personal-area/PersonalArea.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import ListComponent from "../../shared/components/list-component/list-component";
 
 const PersonalArea = () => {
+  const [listItems, setListItems] = useState([]);
+
   return (
     <div className="container">
       <h1>Área personal</h1>
+      <ListComponent items={listItems} canBeEdited />
     </div>
   );
 };
